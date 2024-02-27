ebe826e1d9c532766688bf1e0a9ebc66584f1abb
Svntivgooo <161407742+Svntivgooo@users.noreply.github.com>
2024-02-26 22:27:40 -0500

    Add files via upload

---
+++ Proyecto 1/ModuloUI.py
@@ -0,0 +1,9 @@
+############ Modulo Interfaz ############
+from tabulate import tabulate
+
+def mostrar_keys(results_df):
+    for key in results_df.keys():
+        pass
+print()
+def mostrar_resultados_en_tabla(results_df, resultados):
+    print(results_df[resultados].to_markdown(index=False, headers=["Ciudad de ubicación", "Departamento", "Edad", "Tipo de contagio", "Estado", "País de residencia"]))
