91fb8a50b69a026201189e4620d77c7f1cc3900e
Svntivgooo <161407742+Svntivgooo@users.noreply.github.com>
2024-02-26 22:32:09 -0500

    Delete ModuloAPI.py

--- ModuloAPI.py
+++
@@ -1,9 +0,0 @@
-############ Modulo API ############
-import pandas as pd
-from sodapy import Socrata
-
-def obtener_datos(limite_registros, nombre_departamento):
-    client = Socrata("www.datos.gov.co", "z8hjJYbAY3udhGO55bp3A7hQu")
-    results = client.get("gt2j-8ykr", limit=limite_registros, departamento_nom=nombre_departamento)
-    results_df = pd.DataFrame.from_records(results)
-    return results_df
