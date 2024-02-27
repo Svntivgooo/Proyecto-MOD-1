08d48e07df901594955787137580228102c15e20
Svntivgooo <161407742+Svntivgooo@users.noreply.github.com>
2024-02-27 11:18:28 -0500

    Delete Proyecto 1 directory

--- Proyecto 1/ModuloUI.py
+++
@@ -1,9 +0,0 @@
-############ Modulo Interfaz ############
-from tabulate import tabulate
-
-def mostrar_keys(results_df):
-    for key in results_df.keys():
-        pass
-print()
-def mostrar_resultados_en_tabla(results_df, resultados):
-    print(results_df[resultados].to_markdown(index=False, headers=["Ciudad de ubicación", "Departamento", "Edad", "Tipo de contagio", "Estado", "País de residencia"]))
